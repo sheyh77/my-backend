a50f96bdc2549c536c7e4b305ed261672fa21a15
sheyh77 <ubabaniyozov@gmail.com>
2025-06-17 23:29:39 +0300

    server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log('Foydalanuvchi kiritdi:', username, password);
+
+  // Shu yerda ma'lumotni faylga yozish, database yoki emailga yuborish mumkin
+  res.status(200).json({ message: 'Login ma’lumotlari qabul qilindi' });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ishga tushdi: http://localhost:${PORT}`);
+});
